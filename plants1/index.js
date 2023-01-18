1d4d86d623ab0939ba31d2349c10096dcca003c8
1zemzem <1zemliakova@gmail.com>
2023-01-18 18:01:46 +0300

    feat: add index.js function burgerHandler

--- plants1/index.js
+++ plants1/index.js
@@ -1,3 +1,13 @@
-console.log (
-    "100 баллов"
-)
+const body = document.body;
+const burger = document.getElementById("burger");
+const line = document.querySelectorAll(".burger__field_line");
+burger.addEventListener("click", burgerHandler);
+
+function burgerHandler(e) {
+  e.preventDefault();
+  burger.classList.toggle("burger__field-active");
+  line.forEach((element) => {
+    element.classList.toggle("burger__field_line-active");
+  });
+  body.classList.toggle("noscroll");  
+}
