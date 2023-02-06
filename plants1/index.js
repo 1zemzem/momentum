5b4d5881b17e313ee5ec7c92b854aff0ed80aaa2
1zemzem <1zemliakova@gmail.com>
2023-02-06 17:14:26 +0300

    fix: add scss for section-prices, section-contacts mobile

--- plants1/index.js
+++ plants1/index.js
@@ -18,12 +18,13 @@ const content = document.querySelectorAll(".prices-column__list-paper");
 const pricesPaperHeader = document.querySelectorAll(
   ".prices-column__list-paper-open_header"
 );
+const selectWrapper = document.querySelector(".section-contacts__wrapper")
 const selectHeader = document.querySelectorAll(".select__header");
 const selectBody = document.querySelectorAll(".select__body");
 const selectItem = document.querySelectorAll(".select__item");
 const selectInfo = document.querySelectorAll(".select__contacts-info");
 
-console.log(selectInfo);
+console.log(selectWrapper);
 
 window.onload = function () {
   addServiceButtonsClickHandler();
@@ -150,7 +151,8 @@ function selectChoosen() {
   currentText = select.querySelector(".select__header-title");
   currentText.innerText = text;
   select.classList.remove("select-active");
-  select.classList.add("select-green")
+  select.classList.add("select-green");
+  selectWrapper.classList.add("section-contacts__wrapper-active")
   selectBody.forEach((el) => {
     el.classList.remove("select__body-active");
   });
