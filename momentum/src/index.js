095b3c5fb4c93cb26d57df1ab35504819c98a6f4
1zemzem <1zemliakova@gmail.com>
2023-02-16 18:51:09 +0300

    feat: add bg-slider

--- momentum/src/index.js
+++ momentum/src/index.js
@@ -2,41 +2,125 @@
 import "./style.scss";
 import "./index.html";
 
+const body = document.querySelector(".body");
+const time = document.querySelector(".time");
+const todayDate = document.querySelector(".date");
+const greeting = document.querySelector(".greeting");
+const input = document.querySelector(".name");
+const prevSlider = document.querySelector(".slide-prev");
+const nextSlider = document.querySelector(".slide-next");
+// console.log(nextSlider);
+
+const monthes = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const week = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const dayTime = ["night", "morning", "afternoon", "evening"];
+
 function showTime() {
-  const time = document.querySelector(".time");
-  time.textContent = "Text";
   const date = new Date();
   const currentTime = date.toLocaleTimeString();
-  const currentData = date.toLocaleTimeString();
   time.textContent = currentTime;
   setTimeout(showTime, 1000);
+  setTimeout(getCurrentDate, 1000);
+  setTimeout(showGreeting, 1000);
 }
 showTime();
 
 function getCurrentDate() {
-  const monthes = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  const todayDate = document.querySelector(".date");
-  const date = new Date(Date.now());  
+  const date = new Date();
   const weekdayNumber = date.getDay();
   const weekday = week[weekdayNumber];
   const monthNumber = date.getMonth().toString();
   const month = monthes[monthNumber];
   const day = date.getDate();
-  console.log(month);
   todayDate.innerHTML = `${weekday}, ${month} ${day}`;
 }
-getCurrentDate();
+
+function showGreeting() {
+  const timeOfDay = getTimeOfDay();
+  greeting.innerHTML = `Good ${timeOfDay},`;
+}
+
+function getTimeOfDay() {
+  const date = new Date();
+  const hours = date.getHours();
+  if (hours >= 0 && hours < 6) {
+    return dayTime[0];
+  } else if (hours >= 6 && hours < 12) {
+    return dayTime[1];
+  } else if (hours >= 12 && hours < 18) {
+    return dayTime[2];
+  } else if (hours >= 18 && hours < 24) {
+    return dayTime[3];
+  }
+}
+
+function setLocalStorage() {
+  localStorage.setItem("name", input.value);
+}
+window.addEventListener("beforeunload", setLocalStorage);
+
+function getLocalStorage() {
+  if (localStorage.getItem("name")) {
+    input.value = localStorage.getItem("name");
+  }
+}
+window.addEventListener("load", getLocalStorage);
+
+function getRandomNum() {
+  const random = Math.ceil(Math.random() * 20)
+    .toString()
+    .padStart(2, "0");
+  return random;
+}
+let bgNum = getRandomNum();
+
+function setBg() {
+  const timeOfDay = getTimeOfDay();
+  body.style.backgroundImage = `url("https://raw.githubusercontent.com/1zemzem/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg")`;
+  setTimeout(setBg, 2000);
+}
+setBg();
+
+function getSlideNext() {
+  bgNum = +bgNum;
+  if (bgNum < 20) {
+    bgNum = (bgNum + 1).toString().padStart(2, "0");
+  } else if (bgNum == 20) {
+    bgNum = "1".padStart(2, "0");
+  }
+  setBg(bgNum);
+}
+
+function getSlidePrev() {
+  bgNum = +bgNum;
+  if (bgNum > 1) {
+    bgNum = (bgNum - 1).toString().padStart(2, "0");
+  } else if (bgNum == 1) {
+    bgNum = "20";
+  }
+  setBg(bgNum);
+}
+
+prevSlider.addEventListener("click", getSlidePrev);
+nextSlider.addEventListener("click", getSlideNext);
--- momentum/webpack.config.js
+++ momentum/webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
         use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,        
+        test: /\.(png|svg|jpg|jpeg|ico|gif)$/i,        
         type: "asset/resource",
       },
       {
